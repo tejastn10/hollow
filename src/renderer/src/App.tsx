c17552c8e1afeaabbe3ef1d52a45d565a36da301
Tejas Nikhar <tejastn10@gmail.com>
2025-03-17 19:45:13 +0530

    feat: refactor App component to replace logo and text with a centered Ping button

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,37 +1,24 @@
 import { FC } from "react";
 
-import { Versions } from "./components/Versions";
-
-import electronLogo from "./assets/electron.svg";
+import { Button } from "antd";
 
 const App: FC = () => {
 	const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
 
 	return (
-		<>
-			<img alt="logo" className="logo" src={electronLogo} />
-			<div className="creator">Powered by electron-vite</div>
-			<div className="text">
-				Build an Electron app with <span className="react">React</span>
-				&nbsp;and <span className="ts">TypeScript</span>
-			</div>
-			<p className="tip">
-				Please try pressing <code>F12</code> to open the devTool
-			</p>
-			<div className="actions">
-				<div className="action">
-					<a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-						Documentation
-					</a>
-				</div>
-				<div className="action">
-					<a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-						Send IPC
-					</a>
-				</div>
-			</div>
-			<Versions></Versions>
-		</>
+		<div
+			style={{
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				width: "100%",
+				height: "100%",
+			}}
+		>
+			<Button type="primary" onClick={ipcHandle}>
+				Ping
+			</Button>
+		</div>
 	);
 };
 
