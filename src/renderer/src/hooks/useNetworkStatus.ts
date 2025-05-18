622aaabbbc9e85506eb24626810b6120e53241ad
Tejas Nikhar <tejastn10@gmail.com>
2025-05-18 14:15:43 +0530

    feat: implement useNetworkStatus hook for monitoring network connectivity

---
+++ src/renderer/src/hooks/useNetworkStatus.ts
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+import { NetworkStatus } from "../types/network";
+
+import { NETWORK_STATUS } from "../constants/network";
+
+const useNetworkStatus = (): NetworkStatus => {
+	const [status, setStatus] = useState<NetworkStatus>(NETWORK_STATUS.CONNECTED);
+	const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+	useEffect(() => {
+		const checkNetworkQuality = () => {
+			// ? Checking connectivity
+			if (!navigator.onLine) {
+				console.error("Network is disconnected");
+				setStatus(NETWORK_STATUS.DISCONNECTED);
+				return;
+			}
+
+			if (navigator.onLine) {
+				console.log("Network is connected");
+				setStatus(NETWORK_STATUS.CONNECTED);
+			} else {
+				console.log("Network is weak");
+				setStatus(NETWORK_STATUS.WEAK);
+			}
+		};
+
+		const handleOnline = () => {
+			setIsOnline(true);
+			checkNetworkQuality();
+		};
+
+		const handleOffline = () => {
+			setIsOnline(false);
+			checkNetworkQuality();
+		};
+
+		checkNetworkQuality();
+
+		window.addEventListener("online", handleOnline);
+		window.addEventListener("offline", handleOffline);
+
+		return () => {
+			window.removeEventListener("online", handleOnline);
+			window.removeEventListener("offline", handleOffline);
+		};
+	}, [isOnline]);
+
+	return status;
+};
+
+export { useNetworkStatus };
