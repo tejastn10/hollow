bf439d1c5731b2bf28e7ca78bcc4c62da268243d
Tejas Nikhar <tejastn10@gmail.com>
2025-06-12 16:19:11 +0530

    style: remove redundant comment from useTheme custom hook

--- src/renderer/src/hooks/useTheme.ts
+++ src/renderer/src/hooks/useTheme.ts
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { ThemeContext } from "../theme/ThemeProvider";
 
-// Custom Hook to Use Theme Context
 const useTheme = (): {
 	theme: string;
 	toggleTheme: () => void;
