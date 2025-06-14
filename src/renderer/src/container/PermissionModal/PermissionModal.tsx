f1c7ef5d8919e3f85b0e88c780b8f10e67c6d824
Tejas Nikhar <tejastn10@gmail.com>
2025-06-14 20:27:44 +0530

    feat: add PermissionModal component for password authentication with attempt limit

---
+++ src/renderer/src/container/PermissionModal/PermissionModal.tsx
@@ -0,0 +1,115 @@
+import { FC, useEffect, useState } from "react";
+
+import { App, Form, Input, Modal } from "antd";
+
+import { MAX_PASSWORD_ATTEMPTS } from "./constants";
+
+const { Password } = Input;
+
+interface Props {
+	visible: boolean;
+
+	errorMessage?: string;
+
+	onCancel: () => void;
+	onSubmit: (password: string) => void;
+}
+
+const PermissionModal: FC<Props> = ({ visible, errorMessage, onCancel, onSubmit }) => {
+	const [form] = Form.useForm();
+	const { message } = App.useApp();
+
+	const [attempts, setAttempts] = useState(0);
+	const [isProcessing, setIsProcessing] = useState(false);
+
+	const handleCancel = () => {
+		onCancel();
+
+		form.resetFields();
+
+		setAttempts(0);
+		setIsProcessing(false);
+	};
+
+	const handleSubmit = async ({ password }: { password: string }) => {
+		try {
+			setIsProcessing(true);
+
+			if (password) {
+				onSubmit(password);
+			}
+
+			throw new Error("No password detected!");
+		} catch (error) {
+			console.error(`Error submitting password. Error ${JSON.stringify(error, null, 2)}`);
+			message.error("Failed to submit password. Please try again");
+		} finally {
+			setTimeout(() => {
+				setIsProcessing(false);
+			}, 1000);
+		}
+	};
+
+	useEffect(() => {
+		if (visible) {
+			form.resetFields();
+
+			setAttempts(0);
+		}
+	}, [visible, form]);
+
+	useEffect(() => {
+		if (errorMessage) {
+			setAttempts((prev) => prev + 1);
+
+			message.error(errorMessage);
+			form.setFields([
+				{
+					name: "password",
+					errors: [errorMessage],
+				},
+			]);
+		}
+	}, [errorMessage, message, form]);
+
+	return (
+		<Modal
+			title="Administrator Password Required"
+			open={visible}
+			onOk={form.submit}
+			onCancel={handleCancel}
+			okText="Submit"
+			cancelText="Cancel"
+			destroyOnHidden
+			confirmLoading={isProcessing}
+		>
+			<Form form={form} onFinish={handleSubmit} layout="vertical">
+				<Form.Item
+					name="password"
+					label="Please enter your administrator password to enable real packet capture:"
+					rules={[
+						{ required: true, message: "Password is required" },
+						{
+							validator: async (_, value) => {
+								if (attempts >= MAX_PASSWORD_ATTEMPTS && value) {
+									throw new Error("Too many failed attempts. Please try again later...");
+								}
+							},
+						},
+					]}
+					validateStatus={attempts > 0 ? "error" : undefined}
+					help={attempts > 0 ? `Failed attempts: ${attempts}/${MAX_PASSWORD_ATTEMPTS}` : undefined}
+				>
+					<Password
+						autoFocus
+						onPressEnter={form.submit}
+						placeholder="Enter your password"
+						disabled={attempts >= MAX_PASSWORD_ATTEMPTS}
+					/>
+				</Form.Item>
+			</Form>
+		</Modal>
+	);
+};
+
+export { PermissionModal };
